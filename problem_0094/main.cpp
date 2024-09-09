72c0ed4d4bcfffc23bcbf9bb3551e1cdd2846ef2
Inigo Merckx <inigo@easics.be>
2024-09-09 22:06:43 +0200

    Added some documentation to problem 94

--- problem_0094/main.cpp
+++ problem_0094/main.cpp
@@ -1,36 +1,34 @@
 #include "timer.hpp"
-#include "prettyprint.hpp"
 
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
-#include <vector>
 #include <utility>
 
 /*
 Analysis:
 
-We will try to solve the problem with Pythagorian triples:
+We will try to solve the problem with Pythagorean triples:
 https://en.wikipedia.org/wiki/Pythagorean_triple
 
 First we will calculate the formula for the area of a almost equilateral
 triangle
 
- x /\ x
-  /  \
- /    \
- ------
-  x + 1
+   x /\ x
+    /  \
+   /    \
+   ------
+    x + 1
 
-area = 1/4 * (x + 1) * sqrt(4 * x^2 - (x + 1)^2)
+  area = 1/4 * (x + 1) * sqrt(4 * x^2 - (x + 1)^2)
 
- x /\ x
-  /  \
- /    \
- ------
-  x - 1
+   x /\ x
+    /  \
+   /    \
+   ------
+    x - 1
 
-area = 1/4 * (x - 1) * sqrt(4 * x^2 - (x - 1)^2)
+  area = 1/4 * (x - 1) * sqrt(4 * x^2 - (x - 1)^2)
 
 From this we can conclude a couple of things:
 - x must always be odd. Otherwise the division by 4 will result in a
@@ -38,239 +36,182 @@ From this we can conclude a couple of things:
 - The bottom edge will always be even
 - The height must be an integer
 - The triangle consists of two right angled triangles that are the same
-Pythagorean triple
+  Pythagorean triple
+
+We know that if we check all the Pythagorean triples we will also have checked
+all the almost equilateral triangles. Let's say that we have a primitive triple
+x, y, z with:
+
+  x != 2 * z +- 1
+
+Multiplying both sides by a scaler k we can find a non-primitive solution:
+
+  k * x = 2 * k * z +- 1
+  k * x - 2 * k * z = +- 1
+  k * (x - 2 * z) = +- 1
+  k = +- 1 / (x - 2z)
+
+Which will be fractional. So there is no non-primitive solution.
+
 
 We can use the tree generation method of Pythagorean triples and just check if
 they are adhering to the conditions of an almost equilateral triangle:
 https://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples
 
+Triple: x, y, z
+
+Transform matrices
+
+  a =     x - 2 * y + 2 * z
+  b = 2 * x -     y + 2 * z
+  c = 2 * x - 2 * y + 3 * z
+
+  a = -     x + 2 * y + 2 * z
+  b = - 2 * x +     y + 2 * z
+  c = - 2 * x + 2 * y + 3 * z
+
+  a =     x + 2 * y + 2 * z
+  b = 2 * x +     y + 2 * z
+  c = 2 * x + 2 * y + 3 * z
 
 
-
- Also, as to why you need only two transformations:
-
-You start out with a triangle with sides:
-
-     x2 + y2 = (2x ± 1)2
-     x2 + y2 = 4x2 ± 4x + 1
-     y2 = 3x2 ± 4x + 1
-     y = √3x2 ± 4x + 1
-
-So all triangles fitting the conditions must be of the form:
-
-     x, √3x2 ± 4x + 1, 2x ± 1
-
-The thrid transformation is:
-
-     x2 =  x + 2y + 2z
-     y2 = 2x +  y + 2z
-     z2 = 2x + 2y + 3z
+First we will prove that the 3rd transformation can never produce a valid almost
+equilateral triangle
 
 Starting out with any right triangle of the form:
 
-     x, y, √x2 + y2
-     x2 =  x + 2y + 2√x2 + y2
-     y2 = 2x +  y + 2√x2 + y2
-     z2 = 2x + 2y + 3√x2 + y2
+  x, y, sqrt(x^2 + y^2)
+  a =     x + 2 * y + 2 * sqrt(x^2 + y^2)
+  b = 2 * x +     y + 2 * sqrt(x^2 + y^2)
+  c = 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
 
-And, in any integral case, 2x2 - 1 and 2y2 - 1 are greater than z2:
+It can be shown that 2 * a +- 1 is always greater than c:
 
-     2(x + 2y + 2√x2 + y2)± 1 = 2x + 2y + 3√x2 + y2
-     2x + 4y + 4√x2 + y2 ± 1 = 2x + 2y + 3√x2 + y2
-     2y + √x2 + y2 ± 1 = 0
+  2 * (x + 2 * y + 2 * sqrt(x^2 + y^2)) +- 1 >= 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
+  2 * x + 4 * y + 4 * sqrt(x^2 + y^2) +- 1 >= 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
+  2 * y + sqrt(x^2 + y^2) +- 1 >= 0
 
-And that is impossible.  Also:
+The equation on the left is always larger than 0. Proving the statement
 
-     2(2x + y + 2√x2 + y2) ± 1 = 2x + 2y + 3√x2 + y2
-     4x + 2y + 4√x2 + y2 ± 1 = 2x + 2y + 3√x2 + y2
-     2x + √x2 + y2 ± 1 = 0
+The same be done for proving that 2 * b +- 1 is always greater than c:
 
-Which is also impossible.  Therefore the third transformation is never needed.  The first and second transformations are needed only under certain conditions.  Starting with:
+  2 * (2 * x + y + 2 * sqrt(x^2 + y^2)) +- 1 >= 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
+  4 * x + 2 * y + 4 * sqrt(x^2 + y^2) +- 1 >=  2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
+  2 * x + sqrt(x^2 + y^2) +- 1 >= 0
 
-     x, y, √x2 + y2
-
-And using the first transformation:
-
-     x2 =  x - 2y + 2√x2 + y2
-     y2 = 2x - y - 2√x2 + y2
-     z2 = 2x - 2y + 3√x2 + y2
-
-Assuming that z2 differs from 2x2 or 2y2 by no more than one unit:
-
-     2(x - 2y + 2√x2) ± 1 = 2x - 2y + 3√x2 + y2
-     2x - 4y + 4√x2 ± 1 = 2x - 2y + 3√x2 + y2
-     -2y ± 1 = √x2 + y2
-     (-2y ± 1)2 = x2 + y2
-     4y2 ± 4y + 1 = x2 + y2
-     x2 = 3y2 ± 4y + 1
-     x = √3y2 ± 4y + 1
-
-As stated in the beginning, any right triangle that fits the conditions must be a triangle of the form:
-
-     x, √3x2 ± 4x + 1, 2x ± 1
-
-If 2y2 was the one that differed by no more than one unit instead of 2x2, then:
-
-     2(2x - y - 2√x2 + y2) ± 1 = 2x - 2y + 3√x2 + y2
-     4x - 2y - 4√x2 + y2 ± 1 = 2x - 2y + 3√x2 + y2
-     2x - 7√x2 + y2 = 0
-     2x = 7√x2 + y2
-     4x2 = 7x2 + 7y2
-     7y2 + 3x2 = 0
-
-And that is impossible.  So, before and after the transformation:
-
-     x, √3x2 ± 4x + 1, 2x ± 1
-     √3y2 ± 4y + 1, y, 2y ± 1
-
-Now, applying the first transformation to the result:
+The equation on the left is always larger than 0. Proving the statement
 
 
-     x2 = √3y2 ± 4y + 1 - 2y + 2(2y ± 1)
-     y2 = 2√3y2 ± 4y + 1 - y -2(2y ± 1)
-     z2 = 2√3y2 ± 4y + 1 - 2y + 3(2y ± 1)
+Now let's see what happens when we used the first transform starting out with
+any right triangle of the form:
 
-Simplifying x2, y2 and z2:
+  x, y, sqrt(x^2 + y^2)
+  a =     x - 2 * y + 2 * sqrt(x^2 + y^2)
+  b = 2 * x -     y + 2 * sqrt(x^2 + y^2)
+  c = 2 * x - 2 * y + 3 * sqrt(x^2 + y^2)
 
-     x2 = √3y2 ± 4y + 1 - 2y + 2(2y ± 1)  
-     x2 = √3y2 ± 4y + 1 - 2y + 4y ± 2  
-     x2 = √3y2 ± 4y + 1 + 2y ± 2  
+If the new triple is part of an almost equilateral triangle than z differs from
+2 * a or 2 * b by no more than one unit. Let's first check if 2 * b +- 1 = c is
+possible:
 
-     y2 = 2√3y2 ± 4y + 1 - y -2(2y ± 1)
-     y2 = 2√3y2 ± 4y + 1 - y -4y ± 2     
-     y2 = 2√3y2 ± 4y + 1 - 5y ± 2
+  2 * (2 * x - y + 2 * sqrt(x^2 + y^2)) +- 1 = 2 * x - 2 * y + 3 * sqrt(x^2 + y^2)
+  4 * x - 2 * y + 4 * sqrt(x^2 + y^2) +- 1 = 2 * x - 2 * y + 3 * sqrt(x^2 + y^2)
+  2 * x + sqrt(x^2 + y^2) +- 1 = 0
 
-     z2 = 2√3y2 ± 4y + 1 - 2y + 3(2y ± 1)
-     z2 = 2√3y2 ± 4y + 1 - 2y + 6y ± 3
-     z2 = 2√3y2 ± 4y + 1 - 4y ± 3
+The equation on the left is always larger than 0. Meaning that after using the
+first transform the, that b can't be the smaller side of an almost equilateral
+triangle
 
-Checking to see if x2 ± 1 is equal to z2:
+Let's continue to check 2 * a +- 1 = c
 
-     2(√3y2 ± 4y + 1 + 2y ± 2) ± 1 =
-     2√3y2 ± 4y + 1 + 4y ± 4 ± 1
+  2 * (x - 2 * y + 2 * sqrt(x^2 + y^2)) +- 1 = 2 * x - 2 * y + 3 * sqrt(x^2 + y^2)
+  2 * x - 4 * y + 4 * sqrt(x^2 + y^2) +- 1 = 2 * x - 2 * y + 3 * sqrt(x^2 + y^2)
+  sqrt(x^2 + y^2) +- 1 = 2 * y
+  sqrt(x^2 + y^2) = 2 * y +- 1
+  x^2 + y^2 = (2 * y +- 1)^2
+  x^2 + y^2 = 4 * y^2 +- 4 * y + 1
+  x^2 = 3 * y^2 +- 4 * y + 1
+  x = sqrt(3 * y^2 +- 4 * y + 1)
 
-This can be equal to:
+Using this extra restriction on the starting triple, we can see that the
+starting triangle should also be an almost equilateral triangle
 
-     2√3y2 ± 4y + 1 + 4y ± 3 
+  x, y, sqrt(x^2 + y^2)
+  sqrt(3 * y^2 +- 4 * y + 1), y, sqrt(4 * y^2 +- 4 * y + 1 + y^2)
+  sqrt(3 * y^2 +- 4 * y + 1), y, sqrt(4 * y^2 +- 4 * y + 1)
+  sqrt(3 * y^2 +- 4 * y + 1), y, sqrt((2 * y +- 1)^2)
+  sqrt(3 * y^2 +- 4 * y + 1), y, 2 * y +- 1
 
-Which is z.  If instead you check to see if y2 ± 1 is equal to z2:
+So the starting triangle should also be an almost equilateral triangle with y as
+the smaller side
 
-     2(2√3y2 ± 4y + 1 - 5y ± 2) ± 1 =
-     4√3y2 ± 4y + 1 - 10y ± 4 ± 1
 
-Which will not be equal to z if y is an integer.  So now we have:
+The same reasoning can be applied to the second transform starting out with
+any right triangle of the form:
 
-     x, √3x2 ± 4x + 1, 2x ± 1
+  x, y, sqrt(x^2 + y^2)
+  a = -     x + 2 * y + 2 * sqrt(x^2 + y^2)
+  b = - 2 * x +     y + 2 * sqrt(x^2 + y^2)
+  c = - 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
 
-Now checking to see if other right triangles yield a solution when transformed via the second solution.  Starting with sides:
+If the new triple is part of an almost equilateral triangle than z differs from
+2 * a or 2 * b by no more than one unit. Let's first check if 2 * a +- 1 = c is
+possible:
 
-     x, y, √x2 + y2
-     x2 = -x + 2y + 2√x2 + y2
-     y2 = -2x + y + 2√x2 + y2
-     z2 = -2x + 2y + 3√x2 + y2
+  2 * (- x + 2 * y + 2 * sqrt(x^2 + y^2)) +- 1 = - 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
+  - 2 * x + 4 * y + 4 * sqrt(x^2 + y^2) +- 1 = - 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
+  2 * y + sqrt(x^2 + y^2) +- 1 = 0
 
-Checking to see possible solutions in which 2x ± 1 = z:
+The equation on the left is always larger than 0. Meaning that after using the
+first transform the, that a can't be the smaller side of an almost equilateral
+triangle
 
-     2(2y - x + 2√x2 + y2) ± 1 = 2y - 2x + 3√x2 + y2
-     -2x + 4y + 4√x2 + y2 ± 1 = -2x + 2y + 3√x2 + y2
-     2y + √x2 + y2 = 0
+Let's continue to check 2 * b +- 1 = c
 
-So there is no solution possible.  Now checking to see possible solutions in which 2y ± 1 = z:
+  2 * (- 2 * x + y + 2 * sqrt(x^2 + y^2)) +- 1 = - 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
+  - 4 * x + 2 * y + 4 * sqrt(x^2 + y^2) +- 1 = - 2 * x + 2 * y + 3 * sqrt(x^2 + y^2)
+  sqrt(x^2 + y^2) +- 1 = 2 * x
+  sqrt(x^2 + y^2) = 2 * x +- 1
+  x^2 + y^2 = (2 * x +- 1)^2
+  x^2 + y^2 = 4 * x^2 +- 4 * x + 1
+  y^2 = 3 * x^2 +- 4 * x + 1
+  y = sqrt(3 * x^2 +- 4 * x + 1)
 
-     2(y - 2x + 2√x2 + y2) ± 1 = 2y - 2x + 3√x2 + y2
-     -4x + 2y + 4√x2 + y2 ± 1 = -2x + 2y + 3√x2 + y2
-     -2x + √x2 + y2 ± 1 = 0
-     2x ± 1 = √x2 + y2
-     4x2 ± 4x + 1 = x2 + y2
-     3x2 ± 4x + 1 = y2
-     y = √3x2 ± 4x + 1
+Using this extra restriction on the starting triple, we can see that the
+starting triangle should also be an almost equilateral triangle
 
-And that is what we started out with.  So these are indeed the only primitive solutions.  So all primitive Pythagorean triples which satisfy these conditions can be found using this method.  Non-primitive triples satisfying these conditions, however, cannot be found.  Luckily, there are no such triples.  Start with a primitive triple x, y, z.  So:
+  x, y, sqrt(x^2 + y^2)
+  x, sqrt(3 * x^2 +- 4 * x + 1), sqrt(x^2 + 3 * x^2 +- 4 * x + 1)
+  x, sqrt(3 * x^2 +- 4 * x + 1), sqrt(4 * x^2 +- 4 * x + 1)
+  x, sqrt(3 * x^2 +- 4 * x + 1), sqrt((2 * x +- 1)^2)
+  x, sqrt(3 * x^2 +- 4 * x + 1), 2 * x +- 1
 
-       x != 2z ± 1
+So the starting triangle should also be an almost equilateral triangle with y as
+the smaller side
 
-Multiplying both sides by an a to find a non-primitive solution:
 
-       xa = 2az ± 1
-       xa - 2az = ± 1
-       a(x - 2z) = ± 1
-       a = ± 1 / (x - 2z)
-
-Which will be fractional.  So there is no non-primitive solution.  So, starting with 3, 4, 5, you need to apply the second and first transformations in turn.
+In conclusion we can say that we should always start from an almost equilateral
+triangle in order to find the next. Since we start from the triangle 3, 4, 5 we
+can iteratively perform the second and then the first transform to generate all
+the almost equilateral triangles
 */
 
-//bool is_perfect_square(std::uint64_t const n) {
-//  // 0 is considered as a perfect
-//  // square
-//  //if (n == 0) return true;
-//
-//  //std::uint64_t odd = 1;
-//  //while (n > 0) {
-//  //    n -= odd;
-//  //    odd += 2;
-//  //}
-//
-//  //return n == 0;
-//
-//  std::uint64_t const squareRoot = std::floor(std::sqrt(n));
-//  return squareRoot * squareRoot == n;
-//}
-
-std::uint64_t find_almost_equilateral_triangles(std::uint64_t const limit) {
-  std::vector<std::pair<std::uint64_t, std::uint64_t> > triples = {std::make_pair(2, 1)};
+std::uint64_t find_almost_equilateral_triangles1(std::uint64_t const limit) {
   std::uint64_t perimiterSum = 0;
-
-  //while (not triples.empty()) {
-  //  std::pair<std::uint64_t, std::uint64_t> triple = triples.back();
-  //  triples.pop_back();
-  //  std::uint64_t const m = triple.first;
-  //  std::uint64_t const n = triple.second;
-  //  std::uint64_t const a = m * m - n * n;
-  //  std::uint64_t const b = 2 * m * n;
-  //  std::uint64_t const c = m * m + n * n;
-
-  //  std::uint64_t const smallestSide = std::min(a, b);
-
-  //  //std::cout << "triple: " << smallestSide << ", " << std::max(a,b) << ", " << c << '\n';
-
-  //  // Upper limit that may not be exceeded for the worst case triangle
-  //  if (smallestSide + c < limit) {
-  //    triples.emplace_back(2 * m - n, m); // odd, even
-  //    //triples.emplace_back(2 * m + n, m); // odd, even
-  //    triples.emplace_back(m + 2 * n, n); // even, odd
-  //  }
-
-  //  // Check that the sides only differ by 1 for the standard triangle
-  //  // C must be odd for the area to be an integer. So A will come from an even
-  //  // side and will thus always be an integer number and never have a half part
-  //  if (2 * smallestSide == c - 1 or 2 * smallestSide == c + 1) {
-  //    if (2 * (smallestSide + c) <= limit) {
-  //      std::cout << "triangle: " << 2 * smallestSide << ", " << c << ", " << c << '\n';
-  //      perimiterSum += 2 * (smallestSide + c);
-  //    }
-  //  }
-
-  //}
-
   std::pair<std::uint64_t, std::uint64_t> triple = std::make_pair(2, 1);
 
   std::uint64_t a = 3;
   std::uint64_t b = 4;
   std::uint64_t c = 5;
 
+  std::uint64_t smallestSide = std::min(a, b);
   std::uint64_t perimiter = 16;
   bool evenCycle = true;
 
   do {
-    std::uint64_t const smallestSide = std::min(a, b);
-
-    // Check that the sides only differ by 1 for the standard triangle
-    // C must be odd for the area to be an integer. So A will come from an even
-    // side and will thus always be an integer number and never have a half part
-    if (2 * smallestSide == c - 1 or 2 * smallestSide == c + 1) {
-      std::cout << "triangle: " << 2 * smallestSide << ", " << c << ", " << c << '\n';
-      perimiterSum += 2 * (smallestSide + c);
-    }
+    // The sides should only differ by 1 for the standard triangle because we
+    // will only hit almost equilateral triangles
+    perimiterSum += perimiter;
 
     std::uint64_t m = triple.first;
     std::uint64_t n = triple.second;
@@ -289,52 +230,115 @@ std::uint64_t find_almost_equilateral_triangles(std::uint64_t const limit) {
     b = 2 * m * n;
     c = m * m + n * n;
 
+    smallestSide = std::min(a, b);
     perimiter = 2 * (smallestSide + c);
 
     evenCycle = not evenCycle;
   } while (perimiter <= limit);
 
-  //std::cout << "\nBrute force: \n";
-
-
-  //std::uint64_t i = 3;
-  //bool limit_reached = false;
-
-  //while (not limit_reached) {
-
-  //  limit_reached = true;
-
-  //  if (3 * i - 1 <= limit) {
-  //    limit_reached = false;
-  //    std::uint64_t const heigthSquared = 3 * i * i + 2 * i - 1;
-
-  //    if (is_perfect_square(heigthSquared)) {
-  //      std::cout << "triangle: " << i - 1 << ", " << i << ", " << i << '\n';
-  //      perimiterSum += 3 * i - 1;
-  //    }
-  //  }
-
-  //  if (3 * i + 1 <= limit) {
-  //    limit_reached = false;
-  //    std::uint64_t const heigthSquared = 3 * i * i - 2 * i - 1;
-
-  //    if (is_perfect_square(heigthSquared)) {
-  //      std::cout << "triangle: " << i + 1 << ", " << i << ", " << i << '\n';
-  //      perimiterSum += 3 * i + 1;
-  //    }
-  //  }
-
-  //  i += 2;
-  //}
-
   return perimiterSum;
 }
 
-// 1/4*sqrt(4 * a^2 * b^2 - (a^2 + b^2 - c^2)^2)
-// 1/4*sqrt(4 * x^2 * (x - 1)^2 - (x^2 + (x - 1)^2 - x^2)^2)
-// 1/4*sqrt(4 * x^2 * (x - 1)^2 - (x - 1)^4)
-// 1/4*sqrt((4 * x^2 - (x - 1)^2) * (x - 1)^2)
-// 1/4 * (x - 1) * sqrt(4 * x^2 - (x - 1)^2)
+/*
+Analysis:
+
+We can also solve the problem using Pell's equation:
+https://en.wikipedia.org/wiki/Pell%27s_equation
+
+Applying the Pythagorean theorem on the following triangle gives:
+
+   h |\ x
+     | \
+     |  \
+     ----
+  (x +- 1) / 2
+
+  x^2 = h^2 + ((x +- 1) / 2)^2
+  x^2 = h^2 + (x^2 +- 2 * x + 1) / 4
+  3 * x^2 -+ 2 * x - h^2 = 1
+  3 * x^2 -+ 2 * x - 4 * h^2 = 1
+  9 * x^2 -+ 6 * x - 12 * h^2 = 3
+  (3 * x -+ 1)^2 - 1 - 12 * h^2 = 3
+  (3 * x -+ 1)^2 - 12 * h^2 = 4
+  ((3 * x -+ 1) / 2)^2 - 3 * h^2 = 1
+
+This looks like the Pell's equation:
+
+  a^2 - n * b^2 = 1
+  a = (3 * x -+ 1) / 2
+  b = h
+  n = 3
+
+
+Now we have to prove that finding all integer solutions to this equation will
+give us all the almost equilateral triangles
+
+- We already proved earlier that the height must also be an integer in an almost
+  equilateral triangle
+- If a is an integer then x will also be an integer. This can be seen as
+  follows. If a is a solution then it is not divisible by 3. Otherwise the left
+  side of equation ((3 * x -+ 1) / 2)^2 - 3 * h^2 = 1 becomes divisible by 3 and
+  can never be equal to 1
+
+  Secondly looking at the following equation we can see that for every integer
+  solution a (which is not divisible by 3) we can make it a multiple of 3 by
+  adding or subtracting 1. So we can always find an integer solution for x
+
+  a = (3 * x -+ 1) / 2
+  2 * a +- 1 = 3 * x
+  (2 * a +- 1) / 3 = x
+
+
+From a fundamental solution to Pell's equation we can recursively generation all
+the other integer solutions
+
+The fundamental solution for n = 3 is a = 2 and b = 1. This corresponds to a
+1, 1, 2 triangle
+
+The next values can be created using the following recursive formula:
+
+  a_next = 2 * a + 3 * b
+  b_next = 2 * b + a
+
+
+The formula for the perimeter can then be calculated as follows:
+
+  p = 3 * x +- 1
+  p = 3 * (2 * a +- 1) / 3 +- 1
+  p = 2 * a +- 1 +- 1
+  p = 2 * a +- 2
+
+
+Looking at the values of a we can actually see the it always oscillate between
+1 mod 3 and -1 mod 3. This lets us know that we can just oscillate between doing
+the addition or subtraction every iteration
+
+*/
+
+std::uint64_t find_almost_equilateral_triangles2(std::uint64_t const limit) {
+  std::uint64_t perimiterSum = 0;
+  std::pair<std::uint64_t, std::uint64_t> pellSolution = std::make_pair(7, 4);
+
+  std::uint64_t perimiter = 16;
+  bool evenCycle = false;
+
+  do {
+    perimiterSum += perimiter;
+
+    pellSolution = std::make_pair(2 * pellSolution.first + 3 * pellSolution.second,
+                                  2 * pellSolution.second + pellSolution.first);
+
+    if (evenCycle) {
+      perimiter = 2 * pellSolution.first + 2;
+    } else {
+      perimiter = 2 * pellSolution.first - 2;
+    }
+
+    evenCycle = not evenCycle;
+  } while (perimiter <= limit);
+
+  return perimiterSum;
+}
 
 int main()
 {
@@ -344,7 +348,8 @@ int main()
   {
     timer Timer;
 
-    result = find_almost_equilateral_triangles(limit);
+    //result = find_almost_equilateral_triangles1(limit);
+    result = find_almost_equilateral_triangles2(limit);
   }
 
   std::cout << "Result: " << result << '\n';
